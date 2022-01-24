aafda66856c05ed6fd40b29a774c873fc88f1e99
pajic2017 <pajic.it.2017@gmail.com>
2022-01-24 08:17:14 +0100

    Test after using logger

--- src/git_test_1.java
+++ src/git_test_1.java
@@ -1,6 +1,10 @@
+import java.util.logging.Logger;
+
 public class git_test_1 {
 
     public static void main(String[] args) {
         System.out.println("Test 1");
+        Logger logger = Logger.getLogger(git_test_1.class.getName());
+        logger.info("Test with logger");
     }
 }
