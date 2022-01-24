b26a9dfb48f25d4ba1e2a74b8c7e31ae5b380dc1
pajic2017 <pajic.it.2017@gmail.com>
2022-01-24 08:34:26 +0100

    Test after using Map variable

--- src/git_test_1.java
+++ src/git_test_1.java
@@ -1,10 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 public class git_test_1 {
 
+    public static final Logger logger = Logger.getLogger(git_test_1.class.getName());
+
     public static void main(String[] args) {
         System.out.println("Test 1");
-        Logger logger = Logger.getLogger(git_test_1.class.getName());
         logger.info("Test with logger");
+
+        phonebook.put("John", 123);
+        phonebook.put("Steve", 456);
+
+        logger.info("This show us Map variable: "+phonebook);
+        //logger.info("Another way to use logger: {}", phonebook);
     }
+
+    static Map<String,Integer> phonebook=new HashMap<String,Integer>();
 }
