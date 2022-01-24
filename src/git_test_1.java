044a2b9878f5bd7163f4610b68a86054f13a0031
pajic2017 <pajic.it.2017@gmail.com>
2022-01-24 09:00:45 +0100

    Test change in feature2 devBranch

--- src/git_test_1.java
+++ src/git_test_1.java
@@ -4,6 +4,11 @@ import java.util.logging.Logger;
 
 public class git_test_1 {
 
+    /*constructor without field*/
+    public git_test_1(){
+
+    }
+
     public static final Logger logger = Logger.getLogger(git_test_1.class.getName());
 
     public static void main(String[] args) {
@@ -14,8 +19,10 @@ public class git_test_1 {
         phonebook.put("Steve", 456);
         phonebook.put("Marko",987);
         phonebook.put("Janko",513);
+        phonebook.put("Zoran",681);
         logger.info("This show us Map variable: "+phonebook);
     }
 
+    /*private instance variable*/
     static Map<String,Integer> phonebook=new HashMap<String,Integer>();
 }
